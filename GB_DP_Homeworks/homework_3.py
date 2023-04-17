3df51abc03e329981c126ec7dc431f96ee276381
Juno <polya.alexei@yandex.ru>
2023-04-17 12:24:47 +0300

    All tasks is done in homework_3.py

--- GB_DP_Homeworks/homework_3.py
+++ GB_DP_Homeworks/homework_3.py
@@ -26,11 +26,13 @@
 # ноутбук
 #     12
 
+import random
+
 Menu= '''
 Выберите номер задачи:
 1. Программа для нахождения определенного числа в заданном списке.
 2. Программа для нахождения самого близкого определенного числа в заданном списке.
-3. Программа, которая проверяет счастливость билета.
+3. Программа, которая считает стоимость слова по определенным спискам.
 '''
 print(Menu)
 
@@ -51,12 +53,60 @@ def start():
 
 #Задача 1.
 def task1():
-    pass
+  number = int(input('Введите число N(длина списка): '))
+#   int_list=[1,number+1]
+  desired_number = int(input('Введите искомое число: '))
+  count=0
+  for i in range(1, number+1):
+      if desired_number==i:
+         count += 1
+  print(*range(1,number+1))
+  print(f'{desired_number} -> {count}')    
 
 #Задача 2.
 def task2():
-    pass 
+    
+ user_list_length = int(input('Введите количество элементов в списке: '))
+ user_list = [random.randint(0, user_list_length) for i in range(user_list_length)]
+ print(user_list)
+ user_element_search = int(input('Для какого элемента будем искать ближайшее значение -> '))
+ nearest_element_minimum, nearest_element_maximum = user_element_search - 1, user_element_search + 1
+ flag = True
+ while flag:
+    for i in user_list:
+        if i == nearest_element_minimum:
+            print(f"Ближайший минимальный элемент {i}")
+            flag = False
+            break
+    for i in user_list:
+        if i == nearest_element_maximum:
+            print(f"Ближайший максимальный элемент {i}")
+            flag = False
+            break
+    nearest_element_minimum -= 1
+    nearest_element_maximum += 1 
 
 #Задача 3.
 def task3():
-    pass                
+    
+    english_values = {'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4, 'I': 1, 'J': 8, 'K': 5,
+                  'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3, 'Q': 10, 'R': 1, 'S': 1, 'T': 1, 'U': 1, 'V': 4,
+                  'W': 4, 'X': 8, 'Y': 4, 'Z': 10}
+
+    russian_values = {'А': 1, 'Б': 3, 'В': 1, 'Г': 3, 'Д': 2, 'Е': 1, 'Ё': 3, 'Ж': 5, 'З': 5, 'И': 1, 'Й': 4,
+                  'К': 2, 'Л': 2, 'М': 2, 'Н': 1, 'О': 1, 'П': 2, 'Р': 1, 'С': 1, 'Т': 1, 'У': 2, 'Ф': 10,
+                  'Х': 5, 'Ц': 5, 'Ч': 5, 'Ш': 8, 'Щ': 10, 'Ъ': 10, 'Ы': 4, 'Ь': 3, 'Э': 8, 'Ю': 8, 'Я': 3}
+
+    word = input('Введите слово: ')
+
+    total_score = 0
+
+    for i in word:
+     if i.upper() in english_values:
+        total_score += english_values[i.upper()]
+     elif i.upper() in russian_values:
+        total_score += russian_values[i.upper()]
+    print('Стоимость слова "{}" равна {} очкам.'.format(word, total_score))               
+
+
+start()
